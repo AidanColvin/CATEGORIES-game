ceecc22b8933a47c7dfa2a317b81e6fdd6ab2974
Aidan Colvin <Aidan-Colvin@outlook.com>
2026-02-04 19:34:31 +0000

    Update README with game rules and add categories.py documentation; create master_word_bank.json

--- categories.py
+++ categories.py
@@ -1,4 +1,26 @@
 # LAST UPDATED 01-04-2026
 
+"""
+Categories
+- Each has a different level of difficulty (easy, medium, hard, tricky)
+- Each has a different color (green, yellow, red, purple)
+- Should not be related to spelling or grammar (e.g. "verbs", "nouns", "adjectives", etc.)
+- Should not be related to root of words or adding a letter to the end or beginning of a word (e.g. "cat", "cats", "catty", etc.)
+"""
 
-def 
+# knowledge-bank is a JSON file that contains a list of words and their definitions, as well as a list of categories and the words that belong to each category.
+
+
+
+
+import random
+
+
+# Category 1 (Tricky)
+# Seed category is the first category and is the hardest 
+def seed-category(): 
+
+
+
+
+def generate_knowlage_bank():
