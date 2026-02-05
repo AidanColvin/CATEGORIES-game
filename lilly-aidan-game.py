ed2d040a98f27094991d732cebba83c1e88fc6d8
Aidan Colvin <Aidan-Colvin@outlook.com>
2026-02-04 21:53:35 -0500

    Delete lilly-aidan-game.py

--- lilly-aidan-game.py
+++
@@ -1,5 +0,0 @@
-HTTML
-
-Brighe betweeen Httml and python code
-All catories were we're all on the same line so the categories could merely just be selected based on their level. Additionally, once a category was gotten the appropriate color and the name of the category was not presented to the user. The shuffle function needs to be done before the game starts.
-Tree points 
